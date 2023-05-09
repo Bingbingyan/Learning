20b01fc3a58b9a183426fa9c928ac5fcfa4d0550
晏斌 <bing.b.yan@yzw.cn>
2023-05-09 09:16:44 +0800

    update

---
+++ Python/Streamlit-Learning/Main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import streamlit as st
+from streamlit_multipage import MultiPage
+from pages import s1
+# from pages import s2
+from pages import s3
+from pages import s4
+
+# def my_page(st, **state):
+#     st.title("My Amazing App")
+#     name = st.text_input("Your Name: ")
+#     st.write(f"Hello {name}!")
+#
+#
+# app = MultiPage()
+# app.st = st
+#
+# app.add_app("Hello World", my_page)
+# app.add_app("s1", s1)
+#
+# app.run()
--- Python/Streamlit-Learning/pages/s4.py
+++ Python/Streamlit-Learning/pages/s4.py
@@ -2,17 +2,17 @@
 import streamlit as st
 
 # Store the initial value of widgets in session state
-if "visibility" not in st.session_state:
+if "s1_visibility" not in st.session_state:
     st.session_state.visibility = "visible"
-    st.session_state.disabled = False
+    st.session_state.text_input_disabled = False
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.checkbox("Disable text input widget", key="disabled")
+    st.checkbox("Disable text input widget", key="text_input_disabled")
     st.radio(
         "Set text input label visibility 👉",
-        key="visibility",
+        key="s1_visibility",
         options=["visible", "hidden", "collapsed"],
     )
     st.text_input(
@@ -25,7 +25,7 @@ with col2:
     text_input = st.text_input(
         "Enter some text 👇",
         label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
+        disabled=st.session_state.text_input_disabled,
         placeholder=st.session_state.placeholder,
     )
 
